3d15f32c9a470e28cfa88f8bbca0ef44d79a6fac
Gazar <kolajokimzee2020@gmailcom>
2025-04-09 15:13:22 +0100

    Saving uncommitted changes

--- Menstruation/src/Menstruation.java
+++ Menstruation/src/Menstruation.java
@@ -6,18 +6,51 @@ public class Menstruation {
     private int age;
     private int cycleLength;
     private Date lastPeriodStartDate;
+<<<<<<< HEAD
+=======
+    private String flowDegree;
+>>>>>>> 51280de (Initial commit)
 
     public Menstruation(String name, int age, int cycleLength, Date lastPeriodStartDate) {
         this.name = name;
         this.age = age;
         this.cycleLength = cycleLength;
         this.lastPeriodStartDate = lastPeriodStartDate;
+<<<<<<< HEAD
+=======
+
+    }
+
+
+    public void setUserName(String name) {
+
+        this.name = name;
+    }
+
+    public void setUserAge(int age) {
+
+        this.age = age;
+    }
+
+    public void setCycleLength(int cycleLength) {
+
+        this.cycleLength = cycleLength;
+    }
+
+    public void setLastPeriodStartDate(Date lastPeriodStartDate) {
+
+        this.lastPeriodStartDate = lastPeriodStartDate;
+    }
+    public void setFlowDegree(String flowDegree) {
+        this.flowDegree = flowDegree;
+>>>>>>> 51280de (Initial commit)
     }
 
 
     public String getUserName() {
         return name;
     }
+<<<<<<< HEAD
     public int getUserAge() {
 
         return age;
@@ -46,12 +79,34 @@ public class Menstruation {
 
         this.lastPeriodStartDate = date;
     }
+=======
+
+    public int getUserAge() {
+        return age;
+    }
+
+    public int getCycleLength() {
+        return cycleLength;
+    }
+
+    public Date getLastPeriodStartDate() {
+        return lastPeriodStartDate;
+    }
+
+    public String getFlowDegree() {
+        return flowDegree;
+    }
+
+>>>>>>> 51280de (Initial commit)
 
 
     public static Menstruation collectUserInput() {
         Scanner input = new Scanner(System.in);
         while (true) {
+<<<<<<< HEAD
 
+=======
+>>>>>>> 51280de (Initial commit)
             System.out.print("Enter your name: ");
             String name = input.nextLine();
             if (!name.matches("[A-Za-z]+")) {
@@ -60,10 +115,16 @@ public class Menstruation {
 
             System.out.print("Enter your age: ");
             int age = input.nextInt();
+<<<<<<< HEAD
             if (age < 11 || age > 50 ) {
                 System.out.println("you are too young or too old");
                 continue;
 
+=======
+            if (age < 11 || age > 50) {
+                System.out.println("You are too young or too old");
+                continue;
+>>>>>>> 51280de (Initial commit)
             }
 
             System.out.print("Enter your cycle length in days (e.g., 28): ");
@@ -91,6 +152,7 @@ public class Menstruation {
             System.out.print("  Year: ");
             int year = input.nextInt();
 
+<<<<<<< HEAD
 
 
             Date lastPeriodStartDate = new Date(year - 1900, month - 1, day);
@@ -100,15 +162,53 @@ public class Menstruation {
     }
 
 
+=======
+            System.out.println("Enter your flow degree: (light, moderate,low): )");
+            String flowDegree = input.next().toLowerCase();
+            if(!flowDegree.equals("light")&& !flowDegree.equals("moderate")&& !flowDegree.equals("low")) {
+                System.out.println("Invalid flow degree, write in lower case");
+                flowDegree = input.nextLine().toLowerCase();
+            }
+
+            Date lastPeriodStartDate = new Date(year - 1900, month - 1, day);
+            Menstruation mentral =new Menstruation(name, age, cycleLength, lastPeriodStartDate);
+
+            mentral.setFlowDegree(flowDegree);
+            return mentral;
+        }
+    }
+
+>>>>>>> 51280de (Initial commit)
     public Date calculateNextCycleDate() {
         long milliSecondInaDay = 24L * 60 * 60 * 1000;
         return new Date(lastPeriodStartDate.getTime() + cycleLength * milliSecondInaDay);
     }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 51280de (Initial commit)
     public boolean isLate() {
         Date today = new Date();
         return today.after(calculateNextCycleDate());
     }
+<<<<<<< HEAD
+=======
+    public  void ShowAndCalculateSafeDate(){
+        Date nextPeriodDate = calculateNextCycleDate();
+        long milliSecondInADay = 24L * 60 * 60 * 1000;
+
+        Date ovulationDate = new Date(nextPeriodDate.getTime() -14 * milliSecondInADay);
+        Date UnsafeDaysBeings = new Date(ovulationDate.getTime() - 5 * milliSecondInADay);
+        Date UnsafeDateDaysEnding = new Date(ovulationDate.getTime() -1 * milliSecondInADay);
+
+        System.out.println("------ Safe Date ------");
+        System.out.println("Ovulation date is: " + ovulationDate);
+        System.out.println(" Unsafe day " + UnsafeDaysBeings + " to" + UnsafeDateDaysEnding);
+        System.out.println("Safe Day:before " + UnsafeDaysBeings + "and later " + UnsafeDateDaysEnding );
+
+    }
+>>>>>>> 51280de (Initial commit)
 
     public void showCycleInfo() {
         System.out.println("\n--- Menstruation Information ---");
@@ -116,14 +216,24 @@ public class Menstruation {
         System.out.println("Age: " + age);
         System.out.println("Cycle Length: " + cycleLength + " days");
         System.out.println("Last Period Start Date: " + lastPeriodStartDate);
+<<<<<<< HEAD
         System.out.println("Next Expected Period: " + calculateNextCycleDate());
         System.out.println("Is your period late? " + (isLate() ? "Yes" : "No"));
+=======
+        System.out.println("When to Expect Period next: " + calculateNextCycleDate());
+        System.out.println("Is your period late? " + (isLate() ? "Yes" : "No"));
+        ShowAndCalculateSafeDate();
+
+>>>>>>> 51280de (Initial commit)
     }
 
     public static void main(String[] args) {
         Menstruation userMenstruationCycle = Menstruation.collectUserInput();
         userMenstruationCycle.showCycleInfo();
     }
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> 51280de (Initial commit)
 }
--- Menstruation/test/MenstruationTest.java
+++ Menstruation/test/MenstruationTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MenstruationTest {
     @Test
+<<<<<<< HEAD
     public void testUserGetters() {
         String name = "Ade";
         int age = 13;
@@ -37,9 +38,46 @@ public class MenstruationTest {
         assertEquals(13,period.getUserAge(),"get Age");
         assertEquals(35,period.getCycleLength(),"get days cycle length");
         assertEquals(new Date(),period.getLastPeriodStartDate());
+=======
+    public void testUserSettersAndGetters() {
+        Date date = new Date();
+        Menstruation period = new Menstruation("Name", 0, 0, date);
+
+        period.setUserName("Suliya");
+        period.setUserAge(13);
+        period.setCycleLength(28);
+        period.setLastPeriodStartDate(date);
+
+        assertEquals("Suliya", period.getUserName(), "Name should be 'Ade'");
+        assertEquals(13, period.getUserAge(), "Age should be 13");
+        assertEquals(28, period.getCycleLength(), "Cycle length should be 28");
+        assertEquals(date, period.getLastPeriodStartDate(), "Date should match");
+    }
+    @Test
+    public void testCalculateNextCycleDate() {
+
+        int cycleLength = 28;
+        Date lastPeriodStartDate = new Date();
+
+        Menstruation period = new Menstruation("Ope", 25, cycleLength, lastPeriodStartDate);
+        Date expectedNextCycleDate = new Date(lastPeriodStartDate.getTime() + (long) cycleLength * 24 * 60 * 60 * 1000);
+
+        assertEquals(expectedNextCycleDate, period.calculateNextCycleDate(), "Next cycle date should be 28 days from last start date");
+    }
+    @Test
+    public void testIfMenstruationComesLate() {
+        int cycleLength = 28;
+        Date lastPeriodStartDate = new Date();
+        Menstruation period = new Menstruation("Jane", 25, 0, new Date());
+
+>>>>>>> 51280de (Initial commit)
 
     }
 
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 51280de (Initial commit)
 }
